509fe013392a5e290ecb608610a0e815ba1f54b0
neilien <neil.laravel@gmail.com>
2023-06-29 14:22:09 +0800

    modified react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import Header from "./components/header";
 import Contact from "./components/contact";
 import Footer from "./components/footer";
-
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Contact />
-      </header>
-    </div>
+    <div>
+App    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import Header from './components/header';
 import Footer from './components/footer';
